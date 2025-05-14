e471e4582125776dbda3bbd4f491b7f06916ffcc
Shny <luciano_258456@hotmail.com>
2025-05-14 17:26:56 -0300

    Agregar java project para ejemplos de java

--- java/src/tp_1/GestorEstudiantes.java
+++ java/src/tp_1/GestorEstudiantes.java
@@ -7,51 +7,51 @@ import java.util.TreeSet;
 
 public class GestorEstudiantes {
 	private Set<Estudiante> estudiantes;
-
+	
 	public GestorEstudiantes() {
 		this.estudiantes = new TreeSet<>();
 	}
-
+	
 	public GestorEstudiantes(Estudiante estudiante) {
 		this();
 		this.estudiantes.add(estudiante);
 	}
-
+	
 	public GestorEstudiantes(Estudiante[] estudiantes) {
 		this();
 		for (int i = 0; i < estudiantes.length; i++) {
-			this.estudiantes.add(estudiantes[i]);
+			this.estudiantes.add(estudiantes[i]);			
 		}
 	}
-
+	
 	public GestorEstudiantes(Set<Estudiante> estudiantes) {
 		this();
 		this.estudiantes.addAll(estudiantes);
 	}
-
+	
 	public GestorEstudiantes(List<Estudiante> estudiantes) {
 		this();
 		this.estudiantes.addAll(estudiantes);
 	}
-
+	
 	public Estudiante buscarEstudiantePorDNI(int dni) {
 		for (Estudiante estudiante : this.estudiantes) {
-			if (estudiante.getDni() == dni) {
+			if(estudiante.getDni() == dni) {
 				return estudiante;
 			}
 		}
 		return null;
 	}
-
-	public List<Estudiante> listarEstudiantesPromedioAprobado() {
+	
+	public List<Estudiante> listarEstudiantesPromedioAprobado(){
 		List<Estudiante> estudiantesRet = new ArrayList<>();
-
+		
 		for (Estudiante estudiante : this.estudiantes) {
-			if (estudiante.calcularPromedio() >= 7) {
+			if(estudiante.calcularPromedio() >= 7) {
 				estudiantesRet.add(estudiante);
 			}
 		}
 		return estudiantesRet;
 	}
-
 }
+
