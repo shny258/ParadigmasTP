d1ae5187f876e91b4f0a64936e7085ec3d7545c1
JuliNaspleda <julian.nas@hotmail.com>
2025-05-16 21:36:24 -0300

    fix

--- java/test/tp_1/EstudianteTests.java
+++ java/test/tp_1/EstudianteTests.java
@@ -1,12 +1,7 @@
 package tp_1;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.time.LocalDate;
-
 import org.junit.jupiter.api.Test;
 
 class EstudianteTests {
